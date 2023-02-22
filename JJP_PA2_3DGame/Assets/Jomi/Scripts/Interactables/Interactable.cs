c57f43db87ef6ddfe7e68f265328b1f9d019734a
JoMI103 <a21078@alunos.ipca.pt>
2023-02-22 19:47:19 +0000

    Interactable +

--- JJP_PA2_3DGame/Assets/Jomi/Scripts/Interactables/Interactable.cs
+++ JJP_PA2_3DGame/Assets/Jomi/Scripts/Interactables/Interactable.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public abstract class  Interactable : MonoBehaviour
 {
+    //add or remove an InteractionEvent Component to this gameobject
+    public bool useEvents; 
+
     //Message displayed to player when looking at an interactable
     public string promptMessage;
 
     //this will be called by the player when he interact with the object(subclass)
     public void BaseInteract()
     {
+        if (useEvents)
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
         Interact();
     }
 
