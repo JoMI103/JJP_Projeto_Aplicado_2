4cf87a0f41d3bc2d6c6654e19c2da712a48a361c
G4M30V3R <76963606+G4M30V3R@users.noreply.github.com>
2023-03-06 16:14:52 +0000

    weapons

--- JJP_PA2_3DGame/Assets/Jorge/scripts/GunSystem.cs
+++ JJP_PA2_3DGame/Assets/Jorge/scripts/GunSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GunSystem : MonoBehaviour
 {
@@ -17,6 +18,22 @@ public class GunSystem : MonoBehaviour
     public RaycastHit rayHit;
     public LayerMask isEnemy;
 
+    public GameObject muzzleFlash, bulletHole;
+    public TextMeshProUGUI text;
+
+    public void Awake()
+    {
+        bulletsLeft = magSize;
+        readyToShoot = true;
+    }
+
+    private void Update()
+    {
+        ShootInput();
+
+        text.SetText(bulletsLeft + " / " + magSize);
+    }
+
     private void ShootInput()
     {
         if(allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
@@ -24,23 +41,39 @@ public class GunSystem : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magSize && !reloading) Reload();
 
-        if (readyToShoot && !reloading && bulletsLeft > 0) Shoot();
-        
+        if (readyToShoot && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = bulletsPerClick;
+            Shoot();
+        }
     }
 
     private void Shoot()
     {
         readyToShoot = false;
 
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out rayHit, range, isEnemy))
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0); 
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out rayHit, range, isEnemy))
         {
             Debug.Log(rayHit.collider.name);
             if (rayHit.collider.CompareTag("Enemy"))
                 rayHit.collider.GetComponent<SheepBehaviour>().TakeDamage(damage);
         }
+
+        Instantiate(bulletHole, rayHit.point, Quaternion.Euler(0, 180, 0));
+        Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
         
         bulletsLeft--;
+        bulletsShot--;
+
         Invoke("ResetShot", fireRate);
+
+        if(bulletsShot > 0 && bulletsLeft > 0)
+        Invoke("Shoot", timeBetweenShots);
     }
 
     private void ResetShot()
@@ -50,6 +83,13 @@ public class GunSystem : MonoBehaviour
 
     private void Reload()
     {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
 
+    private void ReloadFinished()
+    {
+        bulletsLeft = magSize;
+        reloading= false;
     }
 }
