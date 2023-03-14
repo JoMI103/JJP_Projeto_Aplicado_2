d18abd8fc7330d1d0fa4862bda87e478e7f08993
G4M30V3R <76963606+G4M30V3R@users.noreply.github.com>
2023-03-14 19:09:06 +0000

    SomeStuff

---
+++ JJP_PA2_3DGame/Assets/Jorge/scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+     
+        }
+    }
+
+    public void Play(string name)
+    {
+        //foreach(Sound s in sounds)
+        //{
+        //    s.source.Play();
+        //}
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s.source.isPlaying) return;
+        s.source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
