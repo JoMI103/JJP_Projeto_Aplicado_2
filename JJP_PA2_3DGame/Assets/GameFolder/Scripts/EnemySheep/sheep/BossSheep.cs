eee1445e2e15ecc3b30db40331e82219af7ebd5b
pedrod765 <80419675+pedrod765@users.noreply.github.com>
2023-04-24 17:57:20 +0100

    ovelhinhasandar

---
+++ JJP_PA2_3DGame/Assets/GameFolder/Scripts/EnemySheep/sheep/BossSheep.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSheep : EnemySheep
+{
+    [Space(10)]
+    [Header("Boss Sheep Atributes")]
+    [Space(10)]
+
+    public bool nothing;
+
+    protected override void MoveAnim()
+    {
+        animator.Play("Walk");
+    }
+    protected override void Attack()
+    {
+
+        placedBuilding.takeDamge(attackDmg);
+    }
+}
